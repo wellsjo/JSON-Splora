7287de71c714ef85805b9a8a910962e4beb94e5f
Wells Johnston <wellsjohnston@gmail.com>
2016-12-03 22:50:04 -0500

    fix welcome message

--- app/js/system/welcome-message.js
+++ app/js/system/welcome-message.js
@@ -4,7 +4,7 @@ module.exports =
 `/**
  * Welcome to JSON-Splora!
  *
- * Drag-and-drop a file, paste a URL, or input raw JSON or JS.
+ * Drag a file, paste a URL, or input JSON in the text editor.
  *
  * When valid JSON is detected, a window will open
  * below where you can run js and jq filters.
